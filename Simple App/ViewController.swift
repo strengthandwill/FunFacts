90c596d3c7d660c2399ed57f1db34054db33e894
3lvintan <elvin@outlook.sg>
2015-09-01 22:44:04 +0800

    Initial Commit

---
+++ Simple App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple App
+//
+//  Created by IDA-GDS-PDD on 1/9/15.
+//  Copyright (c) 2015 IDA-GDS-PDD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
