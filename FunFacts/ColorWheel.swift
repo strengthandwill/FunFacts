ec06090cf98b349b9c1c6ae4f2091d025cfb8ffa
Poh Kah Kong <kahkong87mail@gmail.com>
2015-11-14 19:55:51 +0800

    Update to Famous Quotes

--- FunFacts/ColorWheel.swift
+++ FunFacts/ColorWheel.swift
@@ -11,13 +11,14 @@ import UIKit
 
 struct ColorWheel {
     let colorsArray = [
-        UIColor(red: 90/255.0, green: 187/255.0, blue: 181/255.0, alpha: 1.0), //teal color
-        UIColor(red: 222/255.0, green: 171/255.0, blue: 66/255.0, alpha: 1.0), //yellow color
-        UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0), //red color
-        UIColor(red: 239/255.0, green: 130/255.0, blue: 100/255.0, alpha: 1.0), //orange color
-        UIColor(red: 77/255.0, green: 75/255.0, blue: 82/255.0, alpha: 1.0), //dark color
-        UIColor(red: 105/255.0, green: 94/255.0, blue: 133/255.0, alpha: 1.0), //purple color
-        UIColor(red: 85/255.0, green: 176/255.0, blue: 112/255.0, alpha: 1.0), //green color
+        UIColor(red:0, green:0.63, blue:0.52, alpha:1),
+        UIColor(red:0.1, green:0.69, blue:0.36, alpha:1),
+        UIColor(red:0.13, green:0.5, blue:0.73, alpha:1),
+        UIColor(red:0.56, green:0.25, blue:0.69, alpha:1),
+        UIColor(red:0.96, green:0.62, blue:0, alpha:1),
+        UIColor(red:0.84, green:0.33, blue:0, alpha:1),
+        UIColor(red:0.76, green:0.22, blue:0.14, alpha:1),
+        UIColor(red:0.74, green:0.76, blue:0.78, alpha:1)
     ]
     
     func randomColor() -> UIColor {
